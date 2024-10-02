e011624df0a9876676f57d4ef90c45936938247d
Juan Sebastian Toloza Torres <killsnoobspro@gmail.com>
2024-10-01 21:34:58 -0500

    por terminar

---
+++ ejercicio8.js
@@ -0,0 +1,7 @@
+/**
+ * puntos que se pueden conseguir:
+ * 0.0 = "Puntuacion inaceptable" * 2.400
+ * 0.4 = "Puntuacion Aceptable" * 2.400
+ * 0.6 > + = "Puntuacion meritotio" * 2.400
+ */
+let puntuacion = parseInt(prompt("Ingrese su puntuacion (0.0) (0.4) (0.6)"));
