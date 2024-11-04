b634d98c8688f8a1d3be2bad07ae0651014c0810
junkamilo <juancamilo.campobarrios@gmail.com>
2024-11-03 23:45:54 -0500

    terminado

--- ejercicio71.js
+++ ejercicio71.js
@@ -1,8 +1,27 @@
-let numero = parseFloat(prompt("Ingrese un numero: "));
+let numero = parseFloat(prompt("Ingrese un número: "));
 
-function numero_primo(num) {
-    if(num <= 1){
-        alert(`Èl numero ingresado ${num} no es primo`);
-    }
-    for
-}
+function numeroPrimo(num) {
+            if (num <= 1) {
+                alert(`El número ${num} ingresado no es primo.`);
+                return;
+            }
+            if (num === 2) {
+                alert(`El número ${num} ingresado es primo.`);
+                return;
+            }
+            if (num % 2 === 0) {
+                alert(`El número ${num} ingresado no es primo.`);
+                return;
+            }
+            
+            for (let i = 3; i <= Math.sqrt(num); i += 2) {
+                if (num % i === 0) {
+                    alert(`El número ${num} ingresado no es primo.`);
+                    return;
+                }
+            }
+            
+            alert(`El número ${num} ingresado es primo.`);
+        }
+
+numeroPrimo(numero);
