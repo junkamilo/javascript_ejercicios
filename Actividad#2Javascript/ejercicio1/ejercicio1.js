6b7440117a9488ab9746718b8a36bb66edbece54
junkamilo <juancamilo.campobarrios@gmail.com>
2024-11-10 12:28:41 -0500

    cambie nombre

---
+++ Actividad#2Javascript/ejercicio1/ejercicio1.js
@@ -0,0 +1,11 @@
+let numero1 = parseFloat(prompt("Ingrese le primero numero: "));
+let numero2 = parseFloat(prompt("Ingrese el segundo numero: "));
+
+function mayor(A , B) {
+    if(A > B) {
+        alert(`El numero ${A} es mayor que el numero ${B}`);
+    }else if(B  > A){
+        alert(`El numero ${B} es mayor que el numero ${A}`);
+    }
+}
+mayor(numero1,numero2);
